7dfa6d6e620e05b6551757a970140a22dc8af383
hassansiraj97 <61887746+hassansiraj97@users.noreply.github.com>
2024-09-02 15:37:49 +0200

    Sweetviz code added

--- Datamining.py
+++ Datamining.py
@@ -6,6 +6,7 @@ Created on Mon Sep  2 14:33:02 2024
 """
 
 from ucimlrepo import fetch_ucirepo 
+import sweetviz as sv
   
 # fetch dataset 
 regensburg_pediatric_appendicitis = fetch_ucirepo(id=938) 
@@ -19,3 +20,6 @@ print(regensburg_pediatric_appendicitis.metadata)
   
 # variable information 
 print(regensburg_pediatric_appendicitis.variables) 
+
+my_report = sv.analyze(X)
+my_report.show_html() # This will generate an HTML report
